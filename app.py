603442d6679be4bed5a2f65caec3e8a91447c4e0
kokamoto46 <119825792+kokamoto46@users.noreply.github.com>
2024-11-25 14:57:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,17 +12,17 @@ st.title("Dysphagia Screening Application")
 st.write("Enter the following features to predict the value of FILS score.")
 
 # User input fields
-FIM motor = st.number_input("FIM motor", min_value=13.0, max_value=91.0, value=0.0, step=1.0)
-Duration since stroke onset = st.number_input("Duration since stroke onset ", min_value=0.0, max_value=365.0, value=0.0, step=1.0)
-FIM cognition = st.number_input("FIM cognition", min_value=5.0, max_value=35.0, value=0.0, step=1.0)
-Japan Coma Scale = st.number_input("Japan Coma Scale", min_value=0.0, max_value=300.0, value=0.0, step=1.0)
+fim_motor = st.number_input("FIM motor", min_value=13.0, max_value=91.0, value=13.0, step=1.0)
+duration_since_stroke_onset = st.number_input("Duration since stroke onset", min_value=0.0, max_value=365.0, value=0.0, step=1.0)
+fim_cognition = st.number_input("FIM cognition", min_value=5.0, max_value=35.0, value=5.0, step=1.0)
+japan_coma_scale = st.number_input("Japan Coma Scale", min_value=0.0, max_value=300.0, value=0.0, step=1.0)
 
 # Convert input values to DataFrame
 input_data = pd.DataFrame({
-    "FIM motor": [FIM motor],
-    "Duration since stroke onset": [Duration since stroke onset],
-    "FIM cognition": [FIM cognition],
-    "Japan Coma Scale": [Japan Coma Scale]
+    "FIM motor": [fim_motor],
+    "Duration since stroke onset": [duration_since_stroke_onset],
+    "FIM cognition": [fim_cognition],
+    "Japan Coma Scale": [japan_coma_scale]
 })
 
 # Scale the data and select features
